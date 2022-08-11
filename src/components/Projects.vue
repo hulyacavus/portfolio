ce16b38151fece2e34c793004208b4d43fb2cf8f
Hülya Çavuş <hulyaalmory@gmail.com>
2022-08-11 10:07:23 +0200

    first commit

---
+++ src/components/Projects.vue
@@ -0,0 +1,188 @@
+<template>
+    <div>
+        <h1 class="text-center text-gray-800 dark:text-gray-300 pb-20 mt-12 text-5xl font-extrabold">
+            {{$t('Projects')}}
+        </h1>
+        <div class="flex flex-wrap bg-white dark:bg-gray-700 rounded -mx-px overflow-hidden">
+            <!-- 1 -->
+            <div class="w-full hover14 overflow-hidden cursor-pointer xl:w-1/2 border-b border-r">
+                <div>
+                    <a target="_blank" href="https://aincan.com/">
+                        <figure>
+                            <img class="w-full" src="../assets/images/projects/aincan.png" alt="Aincan project">
+                        </figure>
+                    </a>
+                </div>
+            </div>
+            <div class="w-full overflow-hidden xl:w-1/2 border-b ">
+                <div class="flex flex-col justify-center items-center space-y-6 p-8 mt-8">
+                    <div>
+                        <h1 class="text-center text-fakhti font-bold text-2xl">Aincan</h1>
+                        <hr class="my-3 border-2">
+                    </div>
+                    <div>
+                        <p style="max-width:500px" class="text-sm lg:text-base leading-loose tracking-relaxed lg:leading-loose lg:tracking-relaxed text-gray-500 dark:text-gray-300 tracking-relaxed text-center whitespace-pre-line">
+                            {{$t('AincanProject')}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- 2 -->
+            <div class="w-full hover14 overflow-hidden border-b block xl:hidden">
+                <div class="p-4">
+                    <a target="_blank" href="https://semseem.com/">
+                        <figure>
+                            <img class="w-full object-contain" src="../assets/images/projects/semseem.png" alt="Semseem project">
+                        </figure>
+                    </a>
+                </div>
+            </div>
+            <div class="w-full overflow-hidden xl:w-1/2 border-b border-r">
+                <div class="flex flex-col space-y-6 justify-center items-center p-8 mt-8">
+                    <div>
+                        <h1 class="text-center text-fakhti font-bold text-2xl">Semseem</h1>
+                        <hr class="my-3 border-2">
+                    </div>
+                    <div>
+                        <p style="max-width:500px" class="text-sm lg:text-base leading-loose tracking-relaxed lg:leading-loose lg:tracking-relaxed text-gray-500 dark:text-gray-300 tracking-relaxed text-center whitespace-pre-line">
+                            {{$t('SemseemProject')}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full hover14 overflow-hidden xl:w-1/2 border-b hidden xl:block">
+                <div class="p-4">
+                    <a target="_blank" href="https://semseem.com/">
+                        <figure>
+                            <img class="w-full" src="../assets/images/projects/semseem.png" alt="Semseem project">
+                        </figure>
+                    </a>
+                </div>
+            </div>
+            <!-- 3 -->
+            <div class="w-full hover14 overflow-hidden cursor-pointer xl:w-1/2 border-b border-r">
+                <div>
+                    <a target="_blank" href="https://inayah.net/">
+                        <figure>
+                            <img class="w-full" src="../assets/images/projects/inayah.png" alt="Inayah project">
+                        </figure>
+                    </a>
+                </div>
+            </div>
+            <div class="w-full overflow-hidden xl:w-1/2 border-b ">
+                <div class="flex flex-col justify-center items-center space-y-6 p-8 mt-8">
+                    <div>
+                        <h1 class="text-center text-fakhti font-bold text-2xl">Inayah</h1>
+                        <hr class="my-3 border-2">
+                    </div>
+                    <div>
+                        <p style="max-width:500px" class="text-sm lg:text-base leading-loose tracking-relaxed lg:leading-loose lg:tracking-relaxed text-gray-500 dark:text-gray-300 tracking-relaxed text-center whitespace-pre-line">
+                            {{$t('InayahProject')}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- 4 -->
+            <div class="w-full hover14 overflow-hidden xl:w-1/2 border-b xl:hidden">
+                <div class="p-4">
+                    <a target="_blank" href="https://shrzzin.com/">
+                        <figure>
+                            <img class="w-full " src="../assets/images/projects/shrzzin.png" alt="Shrzzin project">
+                        </figure>
+                    </a>
+                </div>
+            </div>
+            <div class="w-full overflow-hidden xl:w-1/2 border-b border-r">
+                <div class="flex flex-col space-y-6 justify-center items-center p-8 mt-8">
+                    <div>
+                        <h1 class="text-center text-fakhti font-bold text-2xl">Shrzzin</h1>
+                        <hr class="my-3 border-2">
+                    </div>
+                    <div>
+                        <p style="max-width:500px" class="text-sm lg:text-base leading-loose tracking-relaxed lg:leading-loose lg:tracking-relaxed text-gray-500 dark:text-gray-300 tracking-relaxed text-center whitespace-pre-line">
+                            {{$t('ShrzzinProject')}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full hover14 overflow-hidden xl:w-1/2 border-b hidden xl:block">
+                <div class="p-4">
+                    <a target="_blank" href="https://shrzzin.com/">
+                        <figure>
+                            <img class="w-full h-full" src="../assets/images/projects/shrzzin.png" alt="Shrzzin project">
+                        </figure>
+                    </a>
+                </div>
+            </div>
+            <!-- 5 -->
+            <div class="w-full hover14 overflow-hidden cursor-pointer xl:w-1/2 border-b border-r">
+                <div>
+                    <a target="_blank" href="https://www.youtube.com/watch?v=GJ825HreLJw">
+                        <figure>
+                            <img class="w-full" src="../assets/images/projects/soto.webp" alt="SOTO">
+                        </figure>
+                    </a>
+                </div>
+            </div>
+            <div class="w-full overflow-hidden xl:w-1/2 border-b ">
+                <div class="flex flex-col justify-center items-center space-y-6 p-8 mt-8">
+                    <div>
+                        <h1 class="text-center text-fakhti font-bold text-2xl">SOTO</h1>
+                        <hr class="my-3 border-2">
+                    </div>
+                    <div>
+                        <p style="max-width:500px" class="text-sm lg:text-base leading-loose tracking-relaxed lg:leading-loose lg:tracking-relaxed text-gray-500 dark:text-gray-300 tracking-relaxed text-center whitespace-pre-line">
+                            {{$t('SOTOProject')}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:'Projects',
+    methods:{
+        goToProjects(){this.$router.push({ name: 'Projects' })}
+    }
+}
+</script>
+
+<style scoped>
+/* Shine */
+.hover14 figure {
+	position: relative;
+}
+.hover14 figure::before {
+	position: absolute;
+	top: 0;
+	left: -75%;
+	z-index: 2;
+	display: block;
+	content: '';
+	width: 50%;
+	height: 100%;
+	background: -webkit-linear-gradient(left, rgba(255,255,255,0) 0%, rgba(255,255,255,.3) 100%);
+	background: linear-gradient(to right, rgba(255,255,255,0) 0%, rgba(255,255,255,.3) 100%);
+	-webkit-transform: skewX(-25deg);
+	transform: skewX(-25deg);
+}
+.hover14 figure:hover::before {
+	-webkit-animation: shine 1.5s;
+	animation: shine 1.5s;
+}
+@-webkit-keyframes shine {
+	100% {
+		left: 125%;
+	}
+}
+@keyframes shine {
+	100% {
+		left: 125%;
+	}
+}
+</style>
---
+++ src/components/Skills.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1 class="text-center text-gray-800 dark:text-gray-300 pb-20 text-5xl font-extrabold">
+      {{$t('iwork')}}
+    </h1>
+    <carousel
+      :perPageCustom="[[320, 1],[900, 3], [1199, 4]]"
+      :mouse-drag="true"
+      :autoplay="true"
+      :paginationSize="0"
+      :loop="true"
+      :speed="800"
+      :autoplayTimeout="3000"
+      :autoplayHoverPause="false"
+    >
+      <slide v-for="(skill, index) in skills" :key="index">
+        <div class="rounded-full flex flex-col items-center justify-center bg-white dark:bg-gray-700 p-8 w-48 h-48 lg:w-52 lg:h-52 mx-6 space-y-6">
+          <img class="w-1/2" :src="skill.image" />
+          <h1 class="text-base dark:text-gray-300">{{ skill.name }}</h1>
+        </div>
+      </slide>
+    </carousel>
+  </div>
+</template>
+
+
+<script>
+import { Carousel, Slide } from 'vue-carousel'
+
+export default {
+    name:'Skills',
+    components: { Carousel, Slide },
+    data(){
+        return{
+            skills: [
+                {name: 'Python', image: require("../assets/images/skills/python.png")},
+                { name: "JavaScript", image: require("../assets/images/skills/javascript.png")},
+                { name: "HTML", image: require("../assets/images/skills/html.png")},
+                { name: "CSS", image: require("../assets/images/skills/css.png")},
+
+                { name: "Vue JS", image: require("../assets/images/skills/vue.png")},
+                { name: "React JS", image: require("../assets/images/skills/reactjs.png")},
+                { name: "Bootstrap", image: require("../assets/images/skills/bootstrap.png")},
+                { name: "Tailwind CSS", image: require("../assets/images/skills/tailwind.png")},
+
+                { name: "Git", image: require("../assets/images/skills/git.png")},
+
+            ],
+        }
+    }
+};
+</script>
